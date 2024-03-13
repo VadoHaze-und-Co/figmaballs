a75c05be25109817c997ce5fc53a0140a91d0f97
H3rzius <h3rzius@gmail.com>
2024-03-13 13:26:05 +0100

    SCRUM-82 add homepage design from mockup to frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {HomepageComponent} from "./homepage/homepage.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'figmaballs';
+
 }
+
+
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import {BaseChartDirective} from "ng2-charts";
+import { ChartConfiguration } from 'chart.js';
+
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [
+    BaseChartDirective
+  ],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent {
+// Doughnut
+  public doughnutChartLabels: string[] = [ 'Warteschlange', "Überfällige Tickets", "Tickets für Heute", "offene Tickets", "Nicht zugewiesen" ];
+  public doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] = [
+    { data: [ 103, 20, 10, 12, 24 ], label: 'Tickets' }
+  ];
+  //selfAssessmentData.datasets[tooltipItem.datasetIndex]
+  public doughnutChartOptions: ChartConfiguration<'doughnut'>['options'] = {
+    responsive: true,
+    plugins: {
+      tooltip: {
+        enabled: true,
+        callbacks: {
+          label: function(tooltipItem) {
+            let label = this.dataPoints[tooltipItem.datasetIndex];
+          }
+        }
+      },
+      legend: {
+        align: "start",
+        labels: {
+          color: "#b0b0b0",
+          font: {
+            size: 12
+          }
+        }
+      }
+    }
+  };
+
+  constructor() {
+
+  }
+}
