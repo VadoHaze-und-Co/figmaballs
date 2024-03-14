590e390b24d9269efa796f893433a7a43dd6210e
Corey El-Attireh <El-Attireh@meko-s.local>
2024-03-14 13:05:52 +0100

    idk, shcon vergessen lol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import {NavBarComponent} from "./nav-bar/nav-bar.component";
 import {DataService} from "./services/data-service";
 import {AddTicketComponent} from "./add-ticket/add-ticket.component";
 import {SideBarComponent} from "./side-bar/side-bar.component";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, AddTicketComponent, DataService, SideBarComponent],
+  imports: [RouterOutlet, NavBarComponent, AddTicketComponent, DataService, SideBarComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,5 +17,15 @@ export class AppComponent {
   title = 'figmaballs';
 
   constructor(public dataService: DataService) {
+    for (let elementsByTagNameKey in document.getElementsByTagName('div')) {
+
+    }
+  }
+
+  public transitionFix() {
+    if (this.dataService.sideBarService.dragging) {
+      return '0s';
+    }
+    return '.2s';
   }
 }
--- src/app/services/side-bar-service.ts
+++ src/app/services/side-bar-service.ts
@@ -5,10 +5,10 @@ import {DataService} from "./data-service";
 export class SideBarService {
 
   private _sideBarWidth = window.innerWidth * .2;
-  private _sideBarWidth_ = 0;
+  private _sideBarWidth_ = 200;
 
-  public fix = false;
-  private dragging = false;
+  public _fix = false;
+  public dragging = false;
 
   constructor(public dataService: DataService) {
     let localSideBarWidth = localStorage.getItem("sideBarWidth");
@@ -27,19 +27,34 @@ export class SideBarService {
       return;
     }
     let min = 200;
-    if ((x > min && x < min + 5) || (x < min && x > min - 5)) {
+    let max = window.innerWidth - 555;
+    if ((x > min && x < min + 5) || (x < min && x > min / 2)) {
       x = min;
     }
-    if (x < min) {
-      this.fix = true;
+    if (x < min - 1) {
       x = 2;
-      this._sideBarWidth_ = min;
+      this.sideBarWidth_ = this._sideBarWidth;
+      this._fix = true;
+    } else {
+      this._fix = false;
     }
-    let width = window.innerWidth - 555;
-    if (x > width) {
-      x = width;
+    if (x > max) {
+      x = max;
     }
     this._sideBarWidth = x;
+  }
+
+  private set sideBarWidth_(sideBarWidth: number) {
+    let x = sideBarWidth;
+    let min = 200;
+    let max = window.innerWidth - 555;
+    if (x > max) {
+      x = max;
+    }
+    if (x < min) {
+      x = min;
+    }
+    this._sideBarWidth_ = x;
   }
 
   public moveMouse(event: any) {
@@ -47,18 +62,15 @@ export class SideBarService {
       return;
     }
     event.cursor = 'col-resize';
-    this.fix = false;
-    this._sideBarWidth_ = this._sideBarWidth;
     this.sideBarWidth = event.x;
   }
 
-  public dblclick() {
-    if (this.fix) {
-      this.sideBarWidth = this._sideBarWidth_;
-      this.fix = false;
-    } else {
+  public set fixx(fix: boolean) {
+    this._fix = fix;
+    if (fix) {
       this.sideBarWidth = 1;
-      this.fix = true;
+    } else {
+      this.sideBarWidth = this._sideBarWidth_;
     }
   }
 
