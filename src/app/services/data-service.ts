e9e8fff368adb65145a1de69326c5adb83d10802
H3rzius <85434334+H3rzius@users.noreply.github.com>
2024-04-03 10:22:36 +0200

    Revert "Scrum 82 design in frontend implementieren"

--- src/app/services/data-service.ts
+++ src/app/services/data-service.ts
@@ -6,14 +6,4 @@ export class DataService {
 
   // Running data
   public tickets: Ticket[] = [];
-
-  public categories: string[] = ["Vertrieb", "Marketing", "Finanzen", "Personalwesen", "Kundendienst", "Forschung und Entwicklung", "Produktion", "Einkauf", "Qualitätskontrolle", "IT (Informationstechnologie)", "Recht", "Logistik", "Geschäftsentwicklung", "Öffentlichkeitsarbeit", "Projektmanagement", "Facility Management", "Compliance", "Risikomanagement", "Humanressourcen", "Beschaffung", "Buchhaltung", "Verwaltung", "Technischer Support", "Design und Kreativität", "Werbung", "Produktmanagement", "Lagerhaltung", "Datenschutz", "Umweltmanagement", "Schulung und Entwicklung", "Unternehmenskommunikation", "Interne Revision", "Geschäftsanalyse", "Gesundheit und Sicherheit", "Vertragsmanagement", "Informationssicherheit", "Softwareentwicklung", "Hardwareentwicklung", "Unternehmensstrategie", "Wissensmanagement", "Verkaufsförderung", "Kundenbeziehungsmanagement", "Produktionsplanung", "Lieferkettenmanagement", "Innovationsmanagement", "Kundenbindung", "Geschäftspartnerschaften", "F & E-Beratung", "Geschäftsprozessoptimierung", "Projektsteuerung und -überwachung"];
-
-  public width() {
-    return window.innerWidth;
-  }
-
-  public height() {
-    return window.innerHeight;
-  }
 }
