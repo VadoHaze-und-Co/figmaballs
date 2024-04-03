f2966739cb09fd9e582e6a04291cf80b4ce9abd5
H3rzius <85434334+H3rzius@users.noreply.github.com>
2024-04-03 10:21:18 +0200

    Revert "add homepage"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
 import {AddTicketComponent} from "./add-ticket/add-ticket.component";
-import {HomepageComponent} from "./homepage/homepage.component";
 
-export const routes: Routes = [
-  { path: 'homepage', component: HomepageComponent },
-  { path: 'ticket-create', component: AddTicketComponent }
-];
+export const routes: Routes = [{ path: 'ticket-create', component: AddTicketComponent }];
