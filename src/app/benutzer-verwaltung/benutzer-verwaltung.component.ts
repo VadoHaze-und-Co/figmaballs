62a5935f7ef17fd8efac9dbe227126abaa76e0e4
KevinMusiol <kevin785@schule.bremen.de>
2024-04-17 11:19:31 +0200

    Hochladen für das Profilbild und abspeichern

--- src/app/benutzer-verwaltung/benutzer-verwaltung.component.ts
+++ src/app/benutzer-verwaltung/benutzer-verwaltung.component.ts
@@ -1,5 +1,5 @@
 import { NgForOf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -10,12 +10,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './benutzer-verwaltung.component.css'
 })
 export class BenutzerVerwaltungComponent {
-
-  vorname: string = '';
-
-  getVorname(value: string) {
-    this.vorname = value;
-    console.log(this.vorname);
+  @ViewChild('inputFile') inputFile: ElementRef | undefined;
+  triggerClick() {
+    // @ts-ignore
+    this.inputFile.nativeElement.click();
   }
 
+  handleFileInput(files: EventTarget | null) {
+    debugger
+    // @ts-ignore
+    let file = files.files.item(0);
+    let reader = new FileReader();
+    reader.onload = (event: any) => {
+      // @ts-ignore
+      document.getElementById('image-file').src = event.target.result;
+    }
+    // @ts-ignore
+    reader.readAsDataURL(file);
+  }
 }
