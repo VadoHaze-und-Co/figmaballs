f21a699bd24e84698604498b193d4f850657c909
Armin Großmann <armingrossmann11@gmail.com>
2024-04-17 12:18:20 +0200

    add ticket list

---
+++ src/app/ticketlist/ticketlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticketlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './ticketlist.component.html',
+  styleUrl: './ticketlist.component.css'
+})
+export class TicketlistComponent {
+
+}
