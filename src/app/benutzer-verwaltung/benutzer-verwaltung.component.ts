4167f83e75dcb142251311ff840c320a62494654
KevinMusiol <kevin785@schule.bremen.de>
2024-04-17 12:52:16 +0200

    überarbeitung Dropdown und speichern

--- src/app/benutzer-verwaltung/benutzer-verwaltung.component.ts
+++ src/app/benutzer-verwaltung/benutzer-verwaltung.component.ts
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './benutzer-verwaltung.component.css'
 })
 export class BenutzerVerwaltungComponent {
+  qualifikationen = ['Qualifikation 1', 'Qualifikation 2', 'Qualifikation 3'];
   @ViewChild('inputFile') inputFile: ElementRef | undefined;
   triggerClick() {
     // @ts-ignore
@@ -27,5 +28,24 @@ export class BenutzerVerwaltungComponent {
     }
     // @ts-ignore
     reader.readAsDataURL(file);
+  }
+  getSelectedQualifikationen(): string[] {
+    const checkboxes = document.querySelectorAll('.dropdown-content input[type="checkbox"]:checked');
+    // @ts-ignore
+    const selectedQualifikationen = Array.from(checkboxes, (checkbox: HTMLInputElement) => checkbox.value);
+    return selectedQualifikationen;
+  }
+
+  saveUser() {
+    let benutzername = (<HTMLInputElement>document.getElementById('benutzername')).value;
+    let vorname = (<HTMLInputElement>document.getElementById('vorname')).value;
+    let nachname = (<HTMLInputElement>document.getElementById('nachname')).value;
+    let anschrift = (<HTMLInputElement>document.getElementById('anschrift')).value;
+    let stadt = (<HTMLInputElement>document.getElementById('stadt')).value;
+    let plz = (<HTMLInputElement>document.getElementById('plz')).value;
+    let qualifikation =this.getSelectedQualifikationen()
+    let isAdmin = (<HTMLInputElement>document.getElementById('isAdmin')).checked;
+    let benutzer = { benutzername, vorname, nachname, anschrift, stadt, plz, qualifikation, isAdmin };
+    console.log(benutzer);
   }
 }
