abde4defc1e55dd92d2ee22ec6b63e45a2ef382b
Armin Großmann <armingrossmann11@gmail.com>
2024-05-08 09:12:46 +0200

    Login Controller hinzugefügt und funktiontüchtig gemacht

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +10,40 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  //username: string;
-  //password: string;
+  public errorMessage: string;
 
-  constructor() { }
+  username: string = '';
+  password: string = '';
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.errorMessage = '';
+  }
+
 
   login() {
     // Hier kannst du die Logik für das Login implementieren, z.B. eine HTTP-Anfrage an einen Server senden
-    //console.log('Benutzername:', this.username);
-    //console.log('Passwort:', this.password);
+    const loginUrl = 'http://localhost:4200/Login';
+
+    const body = {
+      username: this.username,
+      password: this.password
+    };
+
+
+    this.http.post(loginUrl, body).subscribe(
+      (response) => {
+        // Erfolgreiche Antwort vom Server
+        console.log('Erfolgreich eingeloggt:', response);
+        // Hier könntest du eine Weiterleitung oder andere Aktionen durchführen
+        this.router.navigate(['/homepage']);
+      },
+      (error) => {
+        // Fehler beim Anmelden
+        console.error('Fehler beim Einloggen:', error);
+        // Hier könntest du eine Fehlermeldung für den Benutzer anzeigen
+        this.errorMessage = 'Fehler beim Einloggen: Bitte überprüfen Sie Ihre Anmeldeinformationen und versuchen Sie es erneut.';
+      }
+    );
   }
 
 }
